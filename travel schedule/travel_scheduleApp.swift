142bb5cb7ca683e216eb86d70dc5c8d6e72f2e97
Elizabeth <lmazulova@bk.ru>
2025-05-18 15:17:29 +0300

    Initial Commit

---
+++ travel schedule/travel_scheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  travel_scheduleApp.swift
+//  travel schedule
+//
+//  Created by user on 18.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct travel_scheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
